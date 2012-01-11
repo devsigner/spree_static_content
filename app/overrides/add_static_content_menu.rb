6d88a31ab6d7fc5ef190144d7ac2859b679b18e1
Cedric Darricau <cedricdarricau@gmail.com>
2012-01-11 15:51:38 +0100

    Fix Admin Tab + FR translate

---
+++ app/overrides/add_static_content_menu.rb
@@ -0,0 +1,18 @@
+Deface::Override.new(:virtual_path => 'shared/_store_menu',
+                     :name => 'remove_home_link_on_menu',
+                     :remove => "#home-link")
+
+Deface::Override.new(:virtual_path => 'shared/_nav_bar',
+                     :name => 'add_static_content_menu',
+                     :insert_before => "#search-bar",
+                     :text => "<li id='home-link' data-hook><%= link_to t(:home), root_path %></li>
+                               <% Page.header_links.each do |page| %>
+                                <%= raw('<li>'+link_to(page.title, page.link)+'</li>') %>
+                               <% end %>")
+
+Deface::Override.new(:virtual_path => "layouts/spree_application",
+                     :name => "add_static_content_footer",
+                     :replace_contents => "#footer-left[data-hook]",
+                     :text => "<% Page.footer_links.each do |page| %>
+                                <%= link_to(page.title, page.link) %>
+                               <% end %>")
--- app/overrides/static_content_admin_tab.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => "layouts/admin",
-                     :name => "static_content_admin_tab",
-                     :insert_bottom => "[data-hook='admin_tabs']",
-                     :text => "<%= tab(:pages) %>")
